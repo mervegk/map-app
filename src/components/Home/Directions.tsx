ece7221e9f225cee3e1bb9ee12b12730f0dcf820
mervegk <gekemeke123@gmail.com>
2025-04-18 00:58:50 +0300

    change direction

--- src/components/Home/Directions.tsx
+++ src/components/Home/Directions.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useMap, useMapsLibrary } from '@vis.gl/react-google-maps'
+import { RiArrowRightLine } from "react-icons/ri";
 
 type Props = {}
 
@@ -8,7 +9,11 @@ export default function Directions({ }: Props) {
   const routesLibrary = useMapsLibrary("routes")
   const [directionsService, setDirectionsService] = useState<google.maps.DirectionsService>()
   const [directionsRenderer, setDirectionsRenderer] = useState<google.maps.DirectionsRenderer>()
-  const [routes, setRoutes] = useState([])
+  const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([])
+  const [routeIndex, setRouteIndex] = useState(0)
+
+  const selectedRoute = routes[routeIndex]
+  const leg = selectedRoute?.legs[0]
 
   useEffect(() => {
     if (!routesLibrary || !map) return
@@ -23,22 +28,38 @@ export default function Directions({ }: Props) {
 
     directionsService.route({
       origin: 'Nakkaştepe, İstanbul',
-      destination: 'Kadıköy, İstanbul',
+      destination: 'Beylikdüzü, İstanbul',
       travelMode: google.maps.TravelMode.DRIVING,
       provideRouteAlternatives: true
     }).then(res => {
       directionsRenderer.setDirections(res)
+      setRoutes(res.routes)
     })
 
   }, [directionsService, directionsRenderer])
 
-  console.log(directionsService);
+  useEffect(() => {
+    if (!directionsRenderer) return
 
+    directionsRenderer.setRouteIndex(routeIndex)
+  }, [routeIndex, directionsRenderer])
+
+
+  if (!leg) return null
 
   return (
-    <div>
-
-
+    <div className='absolute top-2 right-4 bg-white rounded-lg shadow-lg p-4'>
+      <h3 className='font-bold'>{selectedRoute.summary}</h3>
+      <p className='flex items-center'>
+        {leg.start_address.split(',')[0]} <RiArrowRightLine /> {leg.end_address.split(',')[0]}
+      </p>
+      <p className='font-medium'>Uzaklık: {leg.distance?.text}</p>
+      <p className='font-medium'>Süre: {leg.duration?.text}</p>
+      <ul>
+        {
+          routes.map((route, index) => <li key={index}><button onClick={() => setRouteIndex(index)}>{route.summary}</button></li>)
+        }
+      </ul>
     </div>
   )
 }
--- src/components/Home/MainMap.tsx
+++ src/components/Home/MainMap.tsx
@@ -11,7 +11,7 @@ export default function MainMap({ }: Props) {
   const position = { lat: 41.01750875299681, lng: 28.9709432341656 };
 
   return (
-    <section className='container mx-auto h-full p-2'>
+    <section className='relative w-full h-full'>
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
         <div className='h-[calc(100vh-72px)]'>
           <Map defaultZoom={12}
