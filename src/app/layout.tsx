5bff71a3090a73a8c1a18e191365ff8f12f6e193
mervegk <gekemeke123@gmail.com>
2025-05-09 19:56:03 +0300

    deleted MarkerContext file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Menu/Header";
-import { MarkerColorProvider } from "@/context/MarkerContext";
 import { SavedLocationsProvider } from "@/context/SavedLocationsContext";
 import ReduxProvider from "@/components/ReduxProvider";
 
--- src/types/context/marker.d.ts
+++ src/types/context/marker.d.ts
@@ -2,8 +2,4 @@ interface MarkerContextType {
   background: string;
   borderColor: string;
   glyphColor: string;
-  changeMarkerColor?: (newColor: string) => void;
-  changeBgColor?: (newColor: string) => void;
-  changeBorderColor?: (newColor: string) => void;
-  changeGlyphColor?: (newColor: string) => void;
 }
